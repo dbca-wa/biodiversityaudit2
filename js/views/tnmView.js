60ee2f527699a2b963a24d39922b9bc721231537
sergel <serge.lebreton@dpaw.wa.gov.au>
2015-03-05 13:10:56 +0800

    - Q70 (partial)

--- js/views/tnmView.js
+++ js/views/tnmView.js
@@ -4,14 +4,16 @@ define([
     'backbone',
     'text!templates/tnm.html',
     'app/tableFacade',
+    'views/tnm/Q70View',
     'views/tnm/Q71View'
-], function ($, _, Backbone, template, tables, Q71View) {
+], function ($, _, Backbone, template, tables, Q70View, Q71View) {
 
     return Backbone.View.extend({
         el: '#content',
 
         render: function () {
             this.$el.html(_.template(template, {}));
+            new Q70View().render();
             new Q71View().render();
         }
     });
