850c9a4c0b13ee676a5935a19f275c25a13182af
serge <serge@gaiaresources.com.au>
2015-04-24 11:49:15 +0800

    - select the species tab on assets page rendering

--- js/views/assetsView.js
+++ js/views/assetsView.js
@@ -69,6 +69,7 @@ define([
                 }
                 this.doSelectValue(type, id);
             }
+            $('#species_header').click();
         },
 
         doSelectValue: function (type, id){
