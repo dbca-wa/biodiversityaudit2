7b809a98bc53d8be44da69bf74939f784842dfd7
serge <serge@gaiaresources.com.au>
2016-02-09 12:01:15 +0800

    Updated the map background to use Openstreet tiles instead of the Mapbox example that was broken.

--- js/app/map.js
+++ js/app/map.js
@@ -177,9 +177,8 @@ define([
         };
 
         /* Background layer */
-        L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
-            maxZoom: 18,
-            id: 'examples.map-i875mjb7'
+        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 18
         }).addTo(map);
 
 
