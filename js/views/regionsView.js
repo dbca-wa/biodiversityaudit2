c2a0d23edb2b1cb1e28808f609f36d34fa003139
Jarid Prince <109533655+idabblewith@users.noreply.github.com>
2026-06-16 13:55:55 +0800

    ref: remove stale references

--- js/views/regionsView.js
+++ js/views/regionsView.js
@@ -8,6 +8,17 @@ define([
 	"models/regionModel",
 	"views/region/regionView",
 ], function ($, ScrollTo, _, Backbone, template, map, RegionModel, RegionView) {
+	// Sanitize untrusted input to prevent DOM-based XSS
+	function escapeHtml(str) {
+		if (typeof str !== "string") return str;
+		return str
+			.replace(/&/g, "&amp;")
+			.replace(/</g, "&lt;")
+			.replace(/>/g, "&gt;")
+			.replace(/"/g, "&quot;")
+			.replace(/'/g, "&#x27;");
+	}
+
 	function handleRegionClick(properties, type) {
 		var model = new RegionModel(properties);
 		var view = new RegionView({ model: model });
@@ -29,8 +40,8 @@ define([
 				// warning! if region data is shown before the ibra json data are loaded, we won't have access to it's
 				// name or the url for the profile
 				regionProperties = map.getRegionsProperties()[id] || {
-					REG_NAME: id,
-					SUB_CODE: id,
+					REG_NAME: escapeHtml(id),
+					SUB_CODE: escapeHtml(id),
 				};
 				handleRegionClick(regionProperties, type);
 			}
